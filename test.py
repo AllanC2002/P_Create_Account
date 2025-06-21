fe4c6f3a51c50f4437ec94b94392f3e4b487b296
Allan Correa <134990755+AllanC2002@users.noreply.github.com>
2025-06-20 20:54:30 -0500

    test: to create

--- test.py
+++ test.py
@@ -3,9 +3,9 @@ import requests
 url = "http://127.0.0.1:8080/create_account"
 
 data = {
-    "Name": "sdasdf",
-    "Lastname": "asfasadsdffd",
-    "User_mail": "alladdfs@coorreea",
+    "Name": "test1",
+    "Lastname": "testeo2",
+    "User_mail": "ascorread1",
     "Password": "1234",
     "Id_type": 1,
     "Id_preferences": 1
